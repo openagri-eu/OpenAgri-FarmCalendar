e961755f0a1680b9fd7b5232259dc95eea27966c
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-05-18 14:36:11 +0200

    replacing dummy local dev auth with example of GK auth

--- helper_scripts/bulk_request_example.py
+++ helper_scripts/bulk_request_example.py
@@ -11,33 +11,20 @@ LOGIN_URL = url = f"{GATEKEEPER_BASE_URL}/api/login/"
 SERVICE_BASE_URL = f'{GATEKEEPER_BASE_URL}/api/proxy/farmcalendar'
 API_ENDPOINT = '/api/v1/bulk/animal-lactating-activities'
 API_URL = f'{SERVICE_BASE_URL}{API_ENDPOINT}'
-API_URL = f'http://localhost:8002{API_ENDPOINT}'
 
 
 def get_auth_token(username, passsword):
 
-    # data = {
-    #     "username": username,
-    #     "password": passsword
-    # }
-    # response = requests.post(url, data=data)
-
-    # if response.status_code == 200:
-    #     # Extract tokens from the JSON response
-    #     tokens = response.json()
-    #     token = tokens.get("access")
-
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_calendar.settings')
-    import jwt
-    from django.conf import settings
-    payload = {
-        'user_id': '1',
-        'exp': datetime.utcnow() + timedelta(days=1),
-        'token_type': 'access',
+    data = {
+        "username": username,
+        "password": passsword
     }
+    response = requests.post(url, data=data)
 
-    token = jwt.encode(payload, settings.JWT_SIGNING_KEY, algorithm=settings.JWT_ALG)
+    if response.status_code == 200:
+        # Extract tokens from the JSON response
+        tokens = response.json()
+        token = tokens.get("access")
 
     return token
 
@@ -55,13 +42,21 @@ def send_bulk_request(bulk_data: List[Dict[str, Any]], auth_token):
 
 
 if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <json_file_path>")
+        print("Example: python script.py admin admin farm_calendar_animals_bulk.json")
+        sys.exit(1)
 
-    with open('farm_calendar_animals_bulk.json', 'r') as f:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    json_file_path = sys.argv[3]
+
+    with open(json_file_path, 'r') as f:
         bulk_data = json.load(f)
 
     # Generate token
-    auth_token = get_auth_token('admin', 'admin')
-    # Convert to bulk format
+    auth_token = get_auth_token(username, password)
 
     # Send request
     response = send_bulk_request(bulk_data, auth_token)
